f38a573ea406728e54727d523a9452bc17f7be39
Zeljko <zeljko@zwr.fi>
2014-11-19 16:11:05 +0200

    root users
    The idea is that admin is admin, visa is an owner of all events in
    development (but not an admin) and john is a visitor.
    
    Signed-off-by: Zeljko <zeljko@zwr.fi>

---
+++ db/seeds/admins.rb
@@ -0,0 +1,14 @@
+User.create  name: "root admin",
+             admin: true,
+             password: "admin123",
+             password_confirmation: "admin123",
+             email: "admin@example.com"
+User.create  name: "Visa Virtanen",
+             password: "password",
+             password_confirmation: "password",
+             email: "visa@example.com"
+User.create  name: "John Doe",
+             admin: true,
+             password: "password",
+             password_confirmation: "password",
+             email: "john@example.com"
